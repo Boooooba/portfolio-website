57244ddf80889dfdd24c20d1ef3cf61771aab149
Boooooba <rekhviashviligiorgi1997@gmail.com>
2022-10-13 20:26:19 +0400

    photos relocated

--- src/data.jsx
+++ src/data.jsx
@@ -1,20 +1,20 @@
 export const proj = [
     {
     id: 1,
-    img: '../src/photos/proj-1.png',
+    img: '/assets/photos/proj-1.png',
     link:'https://ecommerce-proj.netlify.app/',
     code: 'https://github.com/Boooooba/ecommerce-portfolio',
     text: 'This shopping cart project was built using Context API, React Hooks and more!'
     },
     {
     id: 2,
-    img: '../src/photos/proj-2.png',
+    img: '/assets/photos/proj-2.png',
     link: 'https://movsearchapp.netlify.app/',
     code: 'https://github.com/Boooooba/movie-portfolio',
     text: 'This solo project was first built with pure Javascript but later I returned to it and rewrote it in React with functional components and React Hooks.'
     },
     {id: 3,
-    img: '../src/photos/proj-3.png',
+    img:  '/assets/photos/proj-3.png',
     link: 'https://travjournal.netlify.app/',
     code: 'https://github.com/Boooooba/journal-portfolio',
     text: 'This app was built as a solo project during the React basics module from the Frontend Developer Career Path on Scrimba.'
